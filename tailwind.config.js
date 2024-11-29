58e557d8ca7eb653c51431e687510df42af6f222
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-29 18:23:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-dark': '#070707',
+        'active': '#6F6969',
+        'nav-bg': '#2C2B2B',
+        'shadow-color': '#FF7B00',
+        'secondary-color': '#1F1F1F82',
+        'primary-color': '#2E251C',
+        'accent-color': '#0097FB',
+      },
+    },
   },
   plugins: [],
 }
-
