279e803d2fabd2eb986a893dff8863893cb6e289
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-29 13:54:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Navbar from './component/Navbar'
+import Cards from './component/Cards'
+import Form from './component/Form'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <section>
+          <div>
+            
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
