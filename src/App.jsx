7675aa0b18bab598c3abb120b66d9666144e2246
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-30 13:53:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import Navbar from './component/Navbar'
 import Cards from './component/Cards'
 import Form from './component/Form'
-import './App.css'
 
 function App() {
 
