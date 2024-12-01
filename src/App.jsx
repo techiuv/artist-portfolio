ae699209623a66e624a2fd1940ec630463cfb94f
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-01 20:40:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <div className="bg-gradient-to-b from-transparent to-[#070707] h-32 w-full absolute bottom-0 left-0"></div>
           </div>
           <div className='absolute h-[100%] w-[100%] text-center flex justify-center items-center text-transparent  top-0 left-0  -z-10 '>
-            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[13vw] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
+            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[4rem] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
           </div>
 
 
@@ -34,13 +34,15 @@ function App() {
 
         <section id='portfolio'>
 
-          <h3 className='text-center mx-auto text-[5rem]  lg:text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
+          <h3 className='text-center mx-auto text-[3rem]  lg:text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
 
           <div className='w-[90%] md:w-[80%] mx-auto'>
 
-            <Cards title={'tittle'} description={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto provident aliquam itaque tempore aliquid aut impedit dolores atque quo, distinctio quasi aperiam. Voluptatem dolor ullam assumenda? Dicta quia accusamus unde.'} year={2024} medium={'charcoal'} path={'/img/'} />
+            <Cards title={'Fallen in Love'} description={"Fallen in Love is a stunning portrayal of a fleeting moment where emotions rise to the surface without words. The eye becomes the storyteller, capturing the intensity of love so deep it dilates the very essence of one's being.The dark, expansive pupil is more than just a biological response; reflecting the sudden rush of emotion when sight meets the beloved." } year={2024} medium={'charcoal'} path={'/artworks/Yuvraj_Fallen_In_Love_11.7inch×16.5inch_charcoal_2024.jpg'} />
 
-            <Cards title={'tittle'} description={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto provident aliquam itaque tempore aliquid aut impedit dolores atque quo, distinctio quasi aperiam. Voluptatem dolor ullam assumenda? Dicta quia accusamus unde.'} year={2024} medium={'charcoal'} path={'/img/'} />
+            <Cards title={'Ephemeral Life'} description={'This artwork depicts a realistic hand sculpted in charcoal, seemingly deteriorating as the top part of the fingers disintegrates into small, textured fragments. The concept highlights the inevitable truth that every birth leads to a deadline, capturing the fragile balance between existence and oblivion.'} year={2024} medium={'charcoal'} path={'/artworks/Yuvraj_The_End_11.7inch×16.5inch_charcoal_2024.jpg'} />
+
+            <Cards title={'Growth'} description={"This artwork shows a tree growing from a tightly clenched stack of rugged, textured hands, symbolizing life's obstacles such as struggles and resistance. Despite these challenges, the tree emerges with resilience, its roots piercing through the fingers, representing the unstoppable spirit of growth."} year={2024} medium={'charcoal'} path={'/artworks/1000014388.jpg'} />
           </div>
 
         </section>
