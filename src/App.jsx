9d2805d27b8da193227800ba15fc5ec68298a1d6
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-01 20:42:37 +0530

    Update UI

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <div className="bg-gradient-to-b from-transparent to-[#070707] h-32 w-full absolute bottom-0 left-0"></div>
           </div>
           <div className='absolute h-[100%] w-[100%] text-center flex justify-center items-center text-transparent  top-0 left-0  -z-10 '>
-            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[4rem] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
+            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[2rem] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
           </div>
 
 
@@ -27,14 +27,14 @@ function App() {
 
 
         <section id='about' className='w-[100vw]'>
-          <p className='text-center w-[90%] md:w-[70%] lg:w-[65%] mx-auto text-sm md:text-2xl lg:text-3xl text-[#BDBDBD] my-10 md:my-16 font-normal'>
+          <p className='text-center w-[90%] md:w-[70%] lg:w-[65%] mx-auto text-xl md:text-2xl lg:text-3xl text-[#BDBDBD] my-10 md:my-16 font-normal'>
             I am a professional artist based in bihar with an expertise of exp specializing in a variety of mediums, including charcoal, oil, acrylic, water colour and pastels. My artistic journey, shaped by self learning that enabled me to develop a unique and expressive style. Each piece I create is crafted with passion, focusing on story telling and evoking emotions in my audience.         
           </p>
         </section>
 
         <section id='portfolio'>
 
-          <h3 className='text-center mx-auto text-[3rem]  lg:text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
+          <h3 className='text-center mx-auto text-[2rem]  lg:text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
 
           <div className='w-[90%] md:w-[80%] mx-auto'>
 
