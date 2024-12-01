5cb25fd67f1bcf07cb970edc1d73451acbab02b8
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-01 18:26:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <div className="bg-gradient-to-b from-transparent to-[#070707] h-32 w-full absolute bottom-0 left-0"></div>
           </div>
           <div className='absolute h-[100%] w-[100%] text-center flex justify-center items-center text-transparent  top-0 left-0  -z-10 '>
-            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[10vw] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
+            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[13vw] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
           </div>
 
 
@@ -34,7 +34,7 @@ function App() {
 
         <section id='portfolio'>
 
-          <h3 className='text-center mx-auto text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
+          <h3 className='text-center mx-auto text-[5rem]  lg:text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
 
           <div className='w-[90%] md:w-[80%] mx-auto'>
 
