660ed920c4d10e4fb2775821e22facc37c7bb743
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-01 18:19:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ function App() {
       <main>
         <section id='home' className='w-[100vw] h-[100vh] relative flex justify-center items-center'>
           <div className='mx-auto relative h-[100%]'>
-            <img src="/img/Firefly 20240729100151 2.png" alt="Artist Yuvraj" className='h-[100%]' />
+            <img src="/img/Firefly 20240729100151 2.png" alt="Artist Yuvraj" className='h-auto md:h-[100%]' />
             <div className="bg-gradient-to-b from-transparent to-[#070707] h-32 w-full absolute bottom-0 left-0"></div>
           </div>
-          <div className='absolute h-[100%] w-[100%] text-[10vw] text-center flex justify-center items-center text-transparent  top-0 left-0  -z-10 '>
-            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[380px] md:text-[700px] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
+          <div className='absolute h-[100%] w-[100%] text-center flex justify-center items-center text-transparent  top-0 left-0  -z-10 '>
+            <h3 className='text-stroke-1 opacity-50 uppercase font-bold text-[15vw] md:text-[20vw] lg:text-[150px] text-gradient-stroke'>Artist&nbsp;&nbsp;&nbsp;Yuvraj</h3>
           </div>
 
 
@@ -27,8 +27,8 @@ function App() {
 
 
         <section id='about' className='w-[100vw]'>
-          <p className='text-center w-[65%] mx-auto text-3xl text-[#BDBDBD] my-16 font-normal'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto provident aliquam itaque tempore aliquid aut impedit dolores atque quo, distinctio quasi aperiam. Voluptatem dolor ullam assumenda? Dicta quia accusamus unde.
+          <p className='text-center w-[90%] md:w-[70%] lg:w-[65%] mx-auto text-sm md:text-2xl lg:text-3xl text-[#BDBDBD] my-10 md:my-16 font-normal'>
+            I am a professional artist based in bihar with an expertise of exp specializing in a variety of mediums, including charcoal, oil, acrylic, water colour and pastels. My artistic journey, shaped by self learning that enabled me to develop a unique and expressive style. Each piece I create is crafted with passion, focusing on story telling and evoking emotions in my audience.         
           </p>
         </section>
 
@@ -36,7 +36,7 @@ function App() {
 
           <h3 className='text-center mx-auto text-[128px] font-bold drop-shadow-md uppercase'>featured Work</h3>
 
-          <div className='w-[80%] mx-auto'>
+          <div className='w-[90%] md:w-[80%] mx-auto'>
 
             <Cards title={'tittle'} description={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto provident aliquam itaque tempore aliquid aut impedit dolores atque quo, distinctio quasi aperiam. Voluptatem dolor ullam assumenda? Dicta quia accusamus unde.'} year={2024} medium={'charcoal'} path={'/img/'} />
 
