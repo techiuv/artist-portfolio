70b2cf8e4d3907520a4bba3f592d1beaa187c171
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-02 19:32:36 +0530

    Nav fix

--- src/component/NavList.jsx
+++ src/component/NavList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavList = ({href}) => {
   return (
     <li>
-      <a href={`#{href}`} className='cursor-pointer text-gray-600 active:text-white hover:text-white bg-secondary-color'>{href}</a>
+      <a href={`#${href}`} className='cursor-pointer text-gray-600 active:text-white hover:text-white bg-secondary-color'>{href}</a>
     </li>
   )
 }
