04049a24e4facb7a23a6585f92c5df6cba53953c
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-02 18:33:32 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,19 +1,9 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-let isConnected = false;
+const MONGO_URI = process.env.MONGO_URI;
 
-const connectDB = async () => {
-  if (isConnected) return;
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = db.connections[0].readyState === 1;
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB Connection Error:', error);
-  }
-};
-
-module.exports = connectDB;
+export default async function connectToDatabase() {
+  if (mongoose.connection.readyState === 1) return mongoose.connection;
+  await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log('Connected to MongoDB');
+}
