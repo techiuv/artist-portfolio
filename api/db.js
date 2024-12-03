2b50f52220d7de3380ccd9b95d8be4d8a1f00a0b
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-03 12:25:28 +0530

    Error resolved

--- api/db.js
+++ api/db.js
@@ -4,6 +4,6 @@ const MONGO_URI = "mongodb+srv://mailyuvraj0317:EtlUb1RLLKq0zj0Q@formdata.ie7e9.
 
 export default async function connectToDatabase() {
   if (mongoose.connection.readyState === 1) return mongoose.connection;
-  await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
   console.log('Connected to MongoDB');
 }
