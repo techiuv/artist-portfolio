97c9fcfe3a129795975998c3159f3c0a2cfaa109
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-03 14:18:52 +0530

    UI fixes

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Cards = ({ path, tittle, year, description, medium }) => {
   return (
-    <div className='w-[100%] flex items-center flex-col md:flex-row justify-center rounded-3xl bg-secondary-color backdrop-blur-2xl p-3 md:p-6 mt-3 md:mt-6'>
+    <div className='w-[100%] flex items-center flex-col md:flex-row justify-center gap-4 rounded-3xl bg-secondary-color backdrop-blur-2xl p-3 md:p-6 mt-3 md:mt-6'>
 
-      <div className='w-[100%] md:w-[30%] rounded-xl md:rounded-3xl object-cover'>
-        <img src={path} alt={tittle} className='' />
+      <div className='w-[100%] md:w-[30%] rounded-xl md:rounded-3xl object-cover p-2'>
+        <img src={path} alt={tittle} className='rounded-xl' />
       </div>
 
 
@@ -16,7 +16,7 @@ const Cards = ({ path, tittle, year, description, medium }) => {
         </div>
 
         <div className='w-[100%]'>
-          <h2 className='font-bold text-2xl my-3 md:text-3xl uppercase'>{tittle}</h2>
+          <h2 className='font-bold text-3xl my-3  uppercase'>{tittle}</h2>
           <p className='text-sm md:text-xl lg:text-2xl my-2 text-[#BDBDBD] font-normal '>{description}</p>
 
         </div>
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -57,24 +57,26 @@ const Form = () => {
   return (
     <form className="w-[90%] md:w-3/4 mx-auto p-4 bg-secondary-color rounded-3xl py-2 lg:py-4 my-11" onSubmit={handleSubmit}>
 
-      <h3 className=' text-white text-center my-2  text-2xl lg:text-4xl font-bold'>Get in touch</h3>
+      <h3 className=' text-white text-center my-2  text-2xl lg:text-4xl font-bold'>Get in Touch</h3>
       <p className='text-[#BDBDBD] text-center font-normal'>You can reach me anytime</p>
-      <div className='py-3 lg:py-2 flex flex-col lg:flex-row gap-4 md:gap-3 justify-between items-center w-[100%] lg:w-[85%] mx-auto'>
+      <div className='py-3 lg:py-2 flex flex-col md:flex-row gap-4 md:gap-3 justify-between items-center w-[100%] lg:w-[85%] mx-auto'>
         <input
           type="text"
           name="firstName"
           placeholder='First Name'
           value={formData.firstName}
           onChange={handleChange}
-          className="w-full md:w-[50%] border px-4 md:px-6 py-2  lg:py-2 rounded-3xl  bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent active:bg-transparent"
+          autoComplete='off'
+          className="w-full md:w-[50%] border px-4 md:px-6 py-3  lg:py-2 rounded-3xl  bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent active:bg-transparent"
         />
         <input
           type="text"
           name="secondName"
           placeholder='Second Name'
+          autoComplete='off'
           value={formData.secondName}
           onChange={handleChange}
-          className="w-full md:w-[50%] border px-4 md:px-6 py-2  lg:py-2 rounded-3xl bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent active:bg-transparent"
+          className="w-full md:w-[50%] border px-4 md:px-6 py-3  lg:py-2 rounded-3xl bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent active:bg-transparent"
         />
 
       </div>
@@ -84,9 +86,10 @@ const Form = () => {
           type="email"
           name="email"
           placeholder='Email'
+          autoComplete='off'
           value={formData.email}
           onChange={handleChange}
-          className="w-full border px-4 md:px-6 py-2 my-2  lg:py-2 rounded-3xl bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent active:bg-transparent"
+          className="w-full border px-4 md:px-6 py-3 my-2  lg:py-2 rounded-3xl bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent active:bg-transparent"
         />
       </div>
       <div className='w-[100%] lg:w-[85%] mx-auto'>
@@ -95,17 +98,22 @@ const Form = () => {
           placeholder='Your Message'
           value={formData.message}
           onChange={handleChange}
-          className="w-full border px-4 md:px-6 py-2 my-2  lg:py-2 rounded-3xl bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent hover:bg-transparent valid:bg-transparent active:bg-transparent"
+          rows={4}
+          className="w-full border px-4 md:px-6 py-3 my-2  lg:py-2 rounded-3xl bg-transparent text-sm lg:text-xl border-primary-color placeholder:text-[#6F6969] text-white outline-none focus:bg-transparent hover:bg-transparent valid:bg-transparent active:bg-transparent"
         />
       </div>
       {error && <p className="text-red-500 text-center" >{error}</p>}
       {success && <p className="text-green-500  text-center">{success}</p>}
       <button
         type="submit"
-        className={` px-5 mx-auto flex justify-center items-center py-2 my-3 bg-accent-color rounded-full w-auto text-white ${loading && 'opacity-50 cursor-not-allowed'}`}
+        className={` px-5 mx-auto flex justify-center items-center py-2 my-3 bg-accent-color rounded-full w-20 text-white ${loading && 'opacity-50 cursor-not-allowed'}`}
         disabled={loading}
       >
-        {loading ? 'Submitting...' : 'Submit'}
+        {loading ? (
+          <div class="w-6 h-6 border-4 border-dotted border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        ) : (
+          'Submit'
+        )}
       </button>
     </form>
   );
