3a2cb8f5f3f700e8c9abd7a8ccc510ef1ec28dfa
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-03 22:27:25 +0530

    Update Cards.jsx

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Cards = ({ path, tittle, year, description, medium }) => {
   return (
-    <div className='w-[100%] flex items-center flex-col md:flex-row justify-center gap-4 rounded-3xl bg-secondary-color backdrop-blur-2xl p-3 md:p-6 mt-3 md:mt-6'>
+    <div className='w-[100%] flex items-center flex-col md:flex-row justify-center gap-4 rounded-3xl bg-secondary-color backdrop-blur-2xl p-3 md:p-6 mt-5 md:mt-10'>
 
-      <div className='w-[100%] md:w-[30%] rounded-xl md:rounded-3xl object-cover p-2'>
-        <img src={path} alt={tittle} className='rounded-xl' />
+      <div className='w-[100%] md:w-[40%] rounded-xl md:rounded-3xl object-cover p-2'>
+        <img src={path} alt={tittle} className='rounded-xl w-[100%] h-auto' />
       </div>
 
 
@@ -17,7 +17,7 @@ const Cards = ({ path, tittle, year, description, medium }) => {
 
         <div className='w-[100%]'>
           <h2 className='font-bold text-3xl my-3  uppercase'>{tittle}</h2>
-          <p className='text-sm md:text-xl lg:text-2xl my-2 text-[#BDBDBD] font-normal '>{description}</p>
+          <p className='text-lg md:text-xl lg:text-2xl my-2 text-[#BDBDBD] font-normal '>{description}</p>
 
         </div>
       </div>
