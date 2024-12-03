a16b037af8523cd995e9816f301027409519dbb3
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-03 12:03:25 +0530

    Bug fixing in backend

---
+++ api/models/FormSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const formSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+});
+
+const FormData = mongoose.models.Form || mongoose.model('Form', formSchema);
+
+export default FormData; 
