050d12ba358f4256bb1be46feac783ec75b3b2cc
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-12-09 20:52:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
         </section>
 
         <section id='about' className='w-[100%]'>
-          <p className='about-para text-center w-[95%] md:w-[70%] lg:w-[65%] mx-auto text-lg md:text-2xl lg:text-3xl text-[#BDBDBD] my-10 md:my-16 font-normal ' >
+          <p className='about-para text-center w-[90%] md:w-[70%] lg:w-[65%] mx-auto text-lg md:text-2xl lg:text-3xl text-[#BDBDBD] my-10 md:my-16 font-normal ' >
             I am a professional artist based in Bihar with an expertise of {experience} experience specializing in a variety of mediums, including charcoal, oil, acrylic, watercolor, and pastels. My artistic journey, shaped by self-learning, enabled me to develop a unique and expressive style. Each piece I create is crafted with passion, focusing on storytelling and evoking emotions in my audience.
           </p>
         </section>
